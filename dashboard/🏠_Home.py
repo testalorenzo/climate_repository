bcab991f73d4063ac9398c8df439d455a0d1a052
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2022-08-26 14:51:56 +0200

    Delete dashboard directory

--- dashboard/🏠_Home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.markdown("# Welcome to the Climate Repository Project Dashboard")
-st.sidebar.markdown("# Home")
-st.markdown('## Contact')
-st.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (m.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
