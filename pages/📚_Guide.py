d4cdd30097e94917e37f5f7e072ad416c2a52ece
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2022-11-07 23:21:59 +0100

    Update 📚_Guide.py

--- pages/📚_Guide.py
+++ pages/📚_Guide.py
@@ -15,5 +15,5 @@ st.markdown("# Guide")
 """
 
 st.sidebar.markdown('## Contacts')
-st.sidebar.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (m.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
+st.sidebar.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (marco.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
 st.sidebar.image("Embeds logo.png", use_column_width=True)
