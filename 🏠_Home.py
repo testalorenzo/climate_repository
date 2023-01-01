50d71df5b452071888deb6779fde76f30ac70040
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-01 19:16:04 +0100

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -34,7 +34,5 @@ Users that want to run our pipelines on their own data are very welcome to reach
 Stay tuned for updates!
 """
 
-st.sidebar.markdown('## Contacts')
-st.sidebar.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (marco.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
 st.sidebar.image("Embeds logo.png", use_column_width=True)
 st.sidebar.image("download.jpeg", use_column_width=True)
