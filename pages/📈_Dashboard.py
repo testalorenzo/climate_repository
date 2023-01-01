779428653b05ffc00355cdad0a980ebf9b44f214
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-01 19:15:34 +0100

    Update 📈_Dashboard.py

--- pages/📈_Dashboard.py
+++ pages/📈_Dashboard.py
@@ -193,7 +193,5 @@ if stop == False:
      data=data,
      file_name= geo_resolution + '_' + source + '_' + variable + weight + '_data.' + download_extension)
     
-st.sidebar.markdown('## Contacts')
-st.sidebar.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (marco.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
 st.sidebar.image("Embeds logo.png", use_column_width=True)
 st.sidebar.image("download.jpeg", use_column_width=True)
