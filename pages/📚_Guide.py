64fb0811a5bd352799d23f655a77d8316469009a
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-01 19:15:51 +0100

    Update 📚_Guide.py

--- pages/📚_Guide.py
+++ pages/📚_Guide.py
@@ -15,7 +15,5 @@ st.warning("Page under construction!", icon="⚠️")
 * *threshold*: cutoff value.
 """
 
-st.sidebar.markdown('## Contacts')
-st.sidebar.markdown('Feel free to send questions, bug reports, documentation issues, and other comments to Marco Gortan (marco.gortan@studbocconi.it) and [Lorenzo Testa](https://testalorenzo.github.io) (l.testa@sssup.it)')
 st.sidebar.image("Embeds logo.png", use_column_width=True)
 st.sidebar.image("download.jpeg", use_column_width=True)
