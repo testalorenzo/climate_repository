42e7b5cc2e00a06f26b8d6b28105c443c382712c
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-01 19:53:37 +0100

    Update 📈_Dashboard.py

--- pages/📈_Dashboard.py
+++ pages/📈_Dashboard.py
@@ -179,8 +179,7 @@ if stop == False:
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
-  c = alt.Chart(chart_data).mark_circle().encode(
-      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+  c = alt.Chart(chart_data).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
   st.altair_chart(c, use_container_width=True)
 
