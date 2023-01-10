e52372bebad8579ffd006e6d216aaa46b8de555e
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-10 19:05:06 +0100

    Update 📈_Dashboard.py

--- pages/📈_Dashboard.py
+++ pages/📈_Dashboard.py
@@ -156,7 +156,6 @@ if stop == False:
       days_by_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31]
       #data = data.groupby(np.arange(data.shape[1])//12, axis=1).mean()
       data = data.groupby(np.arange(data.shape[1])//12, axis=1).apply(np.average)
-      data = 
     data.columns = list(range(starting_year, ending_year + 1))
     data = pd.concat([observations, data], axis=1)
 
