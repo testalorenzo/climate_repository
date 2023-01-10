89c5de7a2d3e889943585be894460be3abe2e628
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-01-10 19:04:33 +0100

    Update 📈_Dashboard.py

--- pages/📈_Dashboard.py
+++ pages/📈_Dashboard.py
@@ -155,7 +155,8 @@ if stop == False:
       data = data.iloc[:, gap:]
       days_by_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31]
       #data = data.groupby(np.arange(data.shape[1])//12, axis=1).mean()
-      data = data.groupby(np.arange(data.shape[1])//12, axis=1).apply(lambda x: np.average(x, weights=days_by_month))
+      data = data.groupby(np.arange(data.shape[1])//12, axis=1).apply(np.average)
+      data = 
     data.columns = list(range(starting_year, ending_year + 1))
     data = pd.concat([observations, data], axis=1)
 
