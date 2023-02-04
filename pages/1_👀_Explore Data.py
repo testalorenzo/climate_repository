589aada7ec4c0959de44ecc79098c33f97a1fa3b
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-02-04 11:36:34 +0100

    Update 1_👀_Explore Data.py

--- pages/1_👀_Explore Data.py
+++ pages/1_👀_Explore Data.py
@@ -245,9 +245,10 @@ if stop is False:
         # 6.2 Plot map
        
         world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-        # data3 = pd.merge(data, world, left_on='iso3', right_on='iso_a3')
+        data_to_merge = data.iloc[:,snapshot-min_year] 
+        data3 = pd.merge(data_to_merge, world, left_on='iso3', right_on='iso_a3')
         fig, ax = plt.subplots(1, 1)
-        world.plot(column='pop_est', ax=ax, legend=True, legend_kwds={'label': str(variable) + "by Country", 'orientation': "horizontal"})
+        data3.plot(column='pop_est', ax=ax, legend=True, legend_kwds={'label': str(variable) + " by Country in " + str(snapshot), 'orientation': "horizontal"})
         st.pyplot(fig=fig)
 
 
