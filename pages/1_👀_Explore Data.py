4a0e40e764138bb7460bdadeb967170abf8053b1
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-02-04 11:26:07 +0100

    Update 1_👀_Explore Data.py

--- pages/1_👀_Explore Data.py
+++ pages/1_👀_Explore Data.py
@@ -245,7 +245,7 @@ if stop is False:
        
         world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
         # data3 = pd.merge(data, world, left_on='iso3', right_on='iso_a3')
-        st.dataframe(world) 
+        world.plot()
 
 
 # Side bar images
