112c96bc6f5e3cdb3cbe90ab7bfa728fde675660
Lorenzo Testa <64597709+testalorenzo@users.noreply.github.com>
2023-02-13 19:00:06 +0100

    Add files via upload

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Weighted Climate Data Repository")
+st.set_page_config(page_title="Weighted Climate Data Repository", page_icon="🌎", initial_sidebar_state="expanded")
 
 st.markdown("# Welcome to the Weighted Climate Data Repository Dashboard!")
 
